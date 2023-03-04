4c5b419b146ea3a917c84e042cb4e7f56c5e4367
Rami Guessab <ramiguessab94@gmail.com>
2023-03-04 13:58:37 +0100

    Improved import

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,10 +1,20 @@
-import { If } from "rprc";
+import { array, IfElse } from "rprc";
 
 function App() {
   return (
-    <If condition={true}>
-      <p>Hello</p>
-    </If>
+    <array.Map
+      array={["a", "b"]}
+      handler={(item, index) => (
+        <IfElse condition={index === 1} key={index}>
+          <IfElse.If>
+            <p>{item}</p>
+          </IfElse.If>
+          <IfElse.Else>
+            <p>Nothing</p>
+          </IfElse.Else>
+        </IfElse>
+      )}
+    />
   );
 }
 
