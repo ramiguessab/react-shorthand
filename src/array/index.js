efaab6a6d89a827cd797c1fcc30feb267c2315ec
Rami Guessab <ramiguessab94@gmail.com>
2023-03-08 00:11:26 +0100

    replaced every with at

--- src/array/index.js
+++ src/array/index.js
@@ -1,5 +1,7 @@
+import React from "react";
+
 export function Map({ array, handler }) {
-  return array.map(handler);
+  return array.map(handler());
 }
 
 export function Find({ array, handler }) {
@@ -10,6 +12,10 @@ export function Filter({ array, handler }) {
   return array.filter(handler);
 }
 
-export function Every({ array, handler }) {
-  return array.every(handler);
+export function At({ array, index, Wrapper }) {
+  return Wrapper === undefined ? (
+    array.at(index)
+  ) : (
+    <Wrapper>{array.at(index)}</Wrapper>
+  );
 }
--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,7 +1,24 @@
-import { Or } from "rprc";
+import { array } from "rprc";
+
+function Hello({ item, id }) {
+  return (
+    <>
+      <h1>{item}</h1>
+      <h2>{id}</h2>
+    </>
+  );
+}
 
 function App() {
-  return <Or array={[null, "1"]} />;
+  return (
+    <array.Map
+      array={[
+        { id: 38478322, item: "2" },
+        { id: 57859089, item: "1" },
+      ]}
+      Wrapper={Hello}
+    />
+  );
 }
 
 export default App;
